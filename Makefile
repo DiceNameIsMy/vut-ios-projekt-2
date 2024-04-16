c36d0b9c7bb733a00feb9fc3b3cc80beccdcfd1d
Nurdaulet <dicenameismy@gmail.com>
2024-04-16 09:04:10 +0200

    add random number generation

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 main:
 	gcc -std=gnu99 -Wall -Wextra -Werror -pedantic src/main.c -o bin/main
+
+run: main
+	./bin/main
--- src/main.c
+++ src/main.c
@@ -21,7 +21,12 @@ void log_action( char *actor, char *message ) {
     log_id++;
 }
 
-void skibus( int capacity, int stop_amount, int mix_time_between_stops ) {
+// Get a random number betwen 0 and the parameter max
+int rand_number( int max ) { return ( rand() % max ) + 1; }
+
+void skibus( int capacity, int stop_amount, int max_time_between_stops ) {
+    printf( "skibus: %i %i %i\n", capacity, stop_amount,
+            max_time_between_stops );
     // TODO: Start
     // TODO: Ride to next stop(wait for some time)
     // TODO: Allow skiers to get in, repeat until all stops were visited
@@ -31,9 +36,13 @@ void skibus( int capacity, int stop_amount, int mix_time_between_stops ) {
     exit( 0 );
 }
 
-void skier( int stop, int max_time_to_get_to_bus ) {
-    sleep( 1 );
-    // TODO: Breakfast+walk to a stop(wait for some random time)
+void skier( int skier_id, int stop, int max_time_to_get_to_stop ) {
+    int time_to_stop = rand_number( max_time_to_get_to_stop );
+
+    usleep( time_to_stop );
+
+    printf( "skier %i on stop %i, %i\n", skier_id, stop, time_to_stop );
+
     // TODO: Wait for a skibus to arrive to a stop
     // TODO: Try to get in, if failed, keep waiting?
     // TODO: Wait for skibus to get to the end
@@ -45,8 +54,8 @@ int main() {
     int skiers_amount = 5;
     int stops_amount = 3;
     int bus_capacity = 2;
-    int max_time_to_get_to_stop = 1;
-    int max_time_between_stops = 1;
+    int max_time_to_get_to_stop = 1000000;
+    int max_time_between_stops = 1000000;
 
     // TODO: validate inputs
 
@@ -54,22 +63,20 @@ int main() {
     if ( skibus_p < 0 ) {
         printf( "Error creating process\n" );
         return 1;
+    } else if ( skibus_p == 0 ) {
+        skibus( bus_capacity, stops_amount, max_time_between_stops );
     }
 
-    if ( skibus_p == 0 )
-        skibus( bus_capacity, stops_amount, max_time_between_stops );
-
     for ( int i = 0; i < skiers_amount; i++ ) {
-        // TODO: Assign a random stop number to a skier
-        int stop = 1;
+        int stop = rand_number( stops_amount );
 
         pid_t skier_p = fork();
         if ( skier_p < 0 ) {
             loginfo( "Creating a skier #%i failed", i );
             exit( 1 );
+        } else if ( skier_p == 0 ) {
+            skier( i, stop, max_time_to_get_to_stop );
         }
-        if ( skier_p == 0 )
-            skier( stop, max_time_to_get_to_stop );
     }
 
     pid_t child_pid;
@@ -78,7 +85,7 @@ int main() {
         if ( child_pid == -1 ) {
             break;
         }
-        loginfo("process %i has ended", child_pid);
+        loginfo( "process %i has ended", child_pid );
     }
 
     return 0;
