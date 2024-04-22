1de6e858c0acbc0dfec8167345e1be9da2eb0ce0
Nurdaulet <dicenameismy@gmail.com>
2024-04-22 17:11:51 +0200

    ref: main.c

--- src/main.c
+++ src/main.c
@@ -9,24 +9,52 @@
 
 enum { ARG_COUNT = 6 };
 
-/// @brief Load command line arguments into *args and validate them. On error,
-/// first encountered error is printed to stderr.
-/// @param args
-/// @param argc
-/// @param argv
-/// @return -1 when error occures. 0 otherwise.
-int load_args( arguments_t *args, int argc, char *argv[] );
+// CLI arguments ordering
+enum {
+    SKIERS = 1,
+    STOPS = 2,
+    BUS_CAPACITY = 3,
+    WALK_TO_STOP = 4,
+    RIDE_TO_STOP = 5
+};
 
-/// @brief Convert a string to integer or exit the program with -1 exit code.
-/// @param arg
-/// @return
+// Program limitations
+const int MAX_SKIERS = 19999;
+const int MAX_STOPS = 10;
+const int MIN_BUS_CAPACITY = 10;
+const int MAX_BUS_CAPACITY = 100;
+const int MAX_WALK_TO_STOP_TIME = 10000;
+const int MAX_RIDE_TO_STOP_TIME = 1000;
+
+/// @brief Enforce that number is within an allowed range. If number is not
+/// within range, prints an error message and exits the program.
+void within_min_max( int val, int min, int max, char *val_name );
+
+/// @brief Convert a string to integer. If string is not converible, print an
+/// error message and exit the program.
 int arg_to_int_or_exit( char *arg );
 
 int main( int argc, char *argv[] ) {
     arguments_t args;
-    if ( load_args( &args, argc, argv ) == -1 ) {
+    if ( argc != ARG_COUNT ) {
+        (void)fprintf( stderr, "not enough arguments\n" );
         return EXIT_FAILURE;
     }
+
+    args.skiers_amount = arg_to_int_or_exit( argv[ SKIERS ] );
+    args.stops_amount = arg_to_int_or_exit( argv[ STOPS ] );
+    args.bus_capacity = arg_to_int_or_exit( argv[ BUS_CAPACITY ] );
+    args.max_walk_to_stop_time = arg_to_int_or_exit( argv[ WALK_TO_STOP ] );
+    args.max_ride_to_stop_time = arg_to_int_or_exit( argv[ RIDE_TO_STOP ] );
+
+    within_min_max( args.skiers_amount, 0, MAX_SKIERS, "L" );
+    within_min_max( args.stops_amount, 0, MAX_STOPS, "Z" );
+    within_min_max( args.bus_capacity, MIN_BUS_CAPACITY, MAX_BUS_CAPACITY,
+                    "K" );
+    within_min_max( args.max_walk_to_stop_time, 0, MAX_WALK_TO_STOP_TIME,
+                    "TL" );
+    within_min_max( args.max_ride_to_stop_time, 0, MAX_RIDE_TO_STOP_TIME,
+                    "TB" );
 
     if ( run_simulation( &args ) == -1 ) {
         return EXIT_FAILURE;
@@ -63,43 +91,10 @@ int arg_to_int_or_exit( char *arg ) {
     return (int)num_long;
 }
 
-int load_args( arguments_t *args, int argc, char *argv[] ) {
-    if ( argc != ARG_COUNT ) {
-        (void)fprintf( stderr, "not enough arguments\n" );
-        return -1;
+void within_min_max( int val, int min, int max, char *val_name ) {
+    if ( min > val || val > max ) {
+        (void)fprintf( stderr, "%s must be bigger than %i and lower than %i\n",
+                       val_name, min, max );
+        exit( EXIT_FAILURE );
     }
-
-    args->skiers_amount = arg_to_int_or_exit( argv[ 1 ] );
-    if ( args->skiers_amount < 0 || args->skiers_amount >= 20000 ) {
-        (void)fprintf( stderr, "L must be in range 0 < X < 20000\n" );
-        return -1;
-    }
-
-    args->stops_amount = arg_to_int_or_exit( argv[ 2 ] );
-    if ( args->stops_amount <= 0 || args->stops_amount > 10 ) {
-        (void)fprintf( stderr, "Z amount must be in range 0 < X <= 10\n" );
-        return -1;
-    }
-
-    args->bus_capacity = arg_to_int_or_exit( argv[ 3 ] );
-    if ( args->bus_capacity < 10 || args->bus_capacity > 100 ) {
-        (void)fprintf( stderr, "K must be in range 10 <= X <= 100\n" );
-        return -1;
-    }
-
-    args->max_walk_to_stop_time = arg_to_int_or_exit( argv[ 4 ] );
-    if ( args->max_walk_to_stop_time < 0 ||
-         args->max_walk_to_stop_time > 10000 ) {
-        (void)fprintf( stderr, "TL must be in range 0 <= X <= 10000\n" );
-        return -1;
-    }
-
-    args->max_ride_to_stop_time = arg_to_int_or_exit( argv[ 5 ] );
-    if ( args->max_ride_to_stop_time < 0 ||
-         args->max_ride_to_stop_time > 1000 ) {
-        (void)fprintf( stderr, "TB must be in range 0 <= X <= 1000\n" );
-        return -1;
-    }
-
-    return 0;
 }
