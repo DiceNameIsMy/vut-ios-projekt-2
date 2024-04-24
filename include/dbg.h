7330d041e16603d14b95ed3de525c48f2543000d
Nurdaulet <dicenameismy@gmail.com>
2024-04-24 08:18:26 +0200

    include pid in log message

--- include/dbg.h
+++ include/dbg.h
@@ -5,7 +5,7 @@
 
 #ifdef DEBUG
 #define loginfo( s, ... ) \
-    fprintf( stderr, "[INF]" __FILE__ ":%u: " s "\n", __LINE__, ##__VA_ARGS__ )
+    fprintf( stderr, "[INF] pid:%i |" __FILE__ ":%u: " s "\n", getpid(), __LINE__, ##__VA_ARGS__ )
 #else
 #define loginfo( s, ... )
 #endif
